0b14b682cec8fd226eed9009307c84a62e544d07
AC3-CORP <ayeman2908@gmail.com>
2026-02-12 12:19:06 -0600

    Drawing Scene Day 2

--- Assignment 1 - Drawing a Scene/sketch.js
+++ Assignment 1 - Drawing a Scene/sketch.js
@@ -11,10 +11,34 @@ function setup() {
   
 }
 function sky() {
+  noStroke();
+  fill("magenta");
+  rect(0, 0, 1500, 1000);
   
+  noStroke();
+  fill("hotPink");
+  rect(0, 500, 1500, 2000);
+
+  noStroke();
+  fill("pink");
+  rect(0, 600, 1500, 2000);
+
+}
+
+function landscapeNearView(){
+  noStroke();
+  fill("tan");
+  ellipse(0, 900, 800, 400);
+  ellipse(200, 900, 800, 400);
+  ellipse(400, 900, 700, 400);
+  ellipse(600, 900, 800, 400);
+  ellipse(800, 900, 800, 400);
 }
 
 function draw() {
   background(255, 116, 0);
+  sky();
+  landscapeNearView();
 }
+
 
