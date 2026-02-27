71bbaad658ab73dce475f57398edc2662f4ba8e9
AC3-CORP <ayeman2908@gmail.com>
2026-02-26 21:12:21 -0600

    Final Update

--- Assignment 1 - Drawing a Scene/sketch.js
+++ Assignment 1 - Drawing a Scene/sketch.js
@@ -1,19 +1,30 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Interactive Scene (Tatooine w Luke Skywalker)
+// Ayeman Islam
+// Feb 11/26
 
+let x, y; //x and y positions for the movable sun
+let cX = -800; //controls the x-pos of cloud 1 that enters the scene from the right side
+let cX1 = -800; //controls the x-pos of cloud 2 that enters the scene from the right side
+let cX2 = 2000; //controls the x-pos of cloud 3 that enters the scene from the left side 
+let cX3 = 2000; //controls the x-pos of cloud 4 that enters the scene from the left side
+
+let diameter = 155;
+
+let lukeX = 300; // x position of Luke Skywalker that can be changed through pressing arrow keys
+let lukeY = 1050; // y position of Luke Skywalker that is fixed and can't be changed
+let speed = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x = windowHeight/2, y = windowWidth/2; // setting the x and y positions of the sun
 }
+
 function sky() {
+  // This function controls the colors of the gradient sky
   let topColor = color(202, 104, 44); 
   let bottomColor = color(255, 201, 175);
   
-  // Looing through every y-pixel
+  // Looping through every y-pixel
   for (let y = 0; y < height; y++) {
     // Calculating the percentage (0.0 to 1.0)
     let n = map(y, 0, height, 0, 1);
@@ -25,54 +36,186 @@ function sky() {
 }
 
 function sun() {
+  // This functions controls the sun through the movement
+  // of the mouse and speed of the following of the pointer
+  x = lerp(x, mouseX, 0.05);
+  y = lerp(y, mouseY, 0.05);
+
   noStroke()
-  fill("white")
-  circle(675, 730, 85);
-  circle(610, 630, 95);
+  fill("orange")
+  circle(x, y, diameter);
 }
- 
+
+function clouds(){
+  // This controls
+  fill("pink");
+  cX += 3; // This controls the speed of the cloud 1
+  if(cX > 2000) cX = -20; //---This resets the animation once it reaces the end
+  ellipse(cX, 490, 210, 110);
+  ellipse(cX + 40, 500, 180, 60);
+  ellipse(cX - 40, 500, 240, 70);
+  ellipse(cX + 20, 500, 160, 100);
+  ellipse(cX - 20, 510, 160, 50);
+  ellipse(cX + 50, 510, 200, 90);
+
+  cX1 += 2; // This controls the speed of the cloud 2
+  if(cX1 > 2000) cX1 = -20; //---This resets the animation once it reaces the end
+  ellipse(cX1, 235, 220, 110);
+  ellipse(cX1 + 40, 230, 180, 60);
+  ellipse(cX1 - 40, 230, 240, 70);
+  ellipse(cX1 + 20, 240, 160, 100);
+  ellipse(cX1 - 20, 240, 160, 50);
+  ellipse(cX1 + 50, 240, 200, 90);
+
+  cX2 += -2; // This controls the speed of the cloud 3
+  if(cX2 < -100) cX2 = 2000; //---This resets the animation once it reaces the end               
+  ellipse(cX2, 90, 110, 100);
+  ellipse(cX2 + 40, 110, 180, 60);
+  ellipse(cX2 - 40, 90, 240, 70);
+  ellipse(cX2 + 20, 90, 160, 100);
+  ellipse(cX2 - 20, 100, 160, 60);
+  ellipse(cX2 - 50, 100, 200, 90);
+
+  cX3 += -3; // This controls the speed of the cloud 4
+  if(cX3 < -100) cX3 = 2000; //---This resets the animation once it reaces the end
+  ellipse(cX3, 330, 210, 110);
+  ellipse(cX3 + 40, 340, 180, 60);
+  ellipse(cX3 - 40, 340, 240, 70);
+  ellipse(cX3 + 20, 350, 160, 100);
+  ellipse(cX3 - 20, 350, 160, 50);
+  ellipse(cX3 + 50, 350, 200, 90);
+}
+
 function mountains() {
+  // This function is the making of the mountains in the distant
+  // of this Tatooine Scene
   noStroke();
-  fill(217, 131, 130);
+  fill(207, 131, 130);
   triangle(0, 1000, 0, 650, 150, 1000);
   triangle(50, 1000, 230, 580, 550, 1000);
   triangle(250, 1000, 455, 705, 650, 1000);
   triangle(0, 1000, 100, 800, 350, 1000);
   triangle(700, 1000, 650, 800, 450, 1000);
   triangle(700, 1000, 650, 800, 850, 1000);
-  triangle(600, 1000, 755, 700, 875, 1000);
-  triangle(655, 1000, 890, 700, 900, 1000);
+  triangle(600, 1000, 805, 700, 1075, 1000);
+  triangle(655, 1000, 1300, 600, 1670, 1000);
+  triangle(655, 1000, 1670, 700, 2700, windowHeight);
+  triangle(655, 1000, 2270, 800, 3400, windowHeight);
+  triangle(0, 1000, 2000, 1000, 3450, windowHeight);
 }
 
 function grounds() {
+  // This function makes the ground/land of the scene
   noStroke();
   fill(196, 107, 82);
-  rect(1, 930, 900, 800);
+  rect(1, 930, windowWidth, 800);
 
   noStroke();
   fill(213, 125, 92);
-  rect(1, 950, 900, 800);
+  rect(1, 950, windowWidth, 800);
 
   noStroke();
   fill(227, 140, 102);
-  rect(1, 970, 900, 800);
+  rect(1, 970, windowWidth, 800);
 } 
 
 function huts(){
+  // This function is the making of the Tatooine Hut
   noStroke();
   fill(197, 97, 97);
   arc(100, 1050, 560, 580, PI, 0); 
   arc(350, 1050, 230, 370, PI, 0);
   rect(50, 650, 10, 200);
-  rect(70, 655, 5, 200);  
+  rect(70, 655, 5, 200); 
+
+  rect(1, 1000, windowWidth, 40);
+  
+}
+
+function moveLuke() {
+  // This function is the controlling of luke in the scene
+  // It allows us to move from left to right
+  if (keyIsDown(LEFT_ARROW)) {
+    lukeX -= speed;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    lukeX += speed;
+  }
+}
+
+function drawLuke() {
+  // This function is the making of the Luke Skywalker 
+  // The scale of the character can be changed 
+  // through the number change in scale with manual inputs
+  
+  push();
+  translate(lukeX, lukeY);
+
+  scale(1.8);
+
+  // Robed body
+  fill(230, 220, 170);
+  rect(-15, -40, 30, 50, 5);
+
+  // Head
+  fill(255, 220, 180);
+  circle(0, -55, 25);
+
+  // Hair
+  fill(180, 150, 60);
+  arc(0, -60, 26, 20, PI, TWO_PI);
+
+  // Lightsaber 
+  fill(81);
+  rect(15, -30, 6, 20);
+
+  // Lightsaber blade
+  fill(0, 200, 255);
+  rect(17, -80, 2, 50);
+
+  pop();
 }
 
 function draw() {
   sky();
   sun();
+  clouds();
+  clouds();
   mountains();
   grounds();
   huts();
+  moveLuke();
+  drawLuke();
+
+  // This controls the size, alignment of my name
+  // It controls the lighting of my name
+  // Giving it a neon light atmosphere
+  fill(0, 150, 255);
+  textAlign(RIGHT, BOTTOM);
+  textSize(32);
+  text("Ayeman", width-10, height-10);
+
+  fill(255);
+  textAlign(RIGHT, BOTTOM);
+  textSize(32);
+  text("Ayeman", width-10, height-10);
+
+
+  // These if-statements control the sky color through the blending mode
+  // The lower the sun in the canvas, the darker it will get.
+  if (y > 700) {                 
+    blendMode(OVERLAY);
+    noStroke();
+    fill(0, 0, 0, 3000);
+    rect(0, 0, width, height);
+    blendMode(BLEND); 
+  }
+  if( y > 900){
+    blendMode(MULTIPLY);
+    noStroke();
+    fill(0, 0, 230, 200);
+    rect(0, 0, width, height);
+    blendMode(BLEND);
+
+  }
 }
-
-
