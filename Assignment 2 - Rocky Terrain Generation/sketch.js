907ba1c2524eca0df30cef5c57a53eba6812ec52
AC3-CORP <ayeman2908@gmail.com>
2026-03-08 14:13:23 -0600

    Added Comments for the necessary functions and variables.

--- Assignment 2 - Rocky Terrain Generation/sketch.js
+++ Assignment 2 - Rocky Terrain Generation/sketch.js
@@ -4,21 +4,22 @@
 //
 // 
 
-//Global Variable
-let y;
-let start, original_start;
-let rectWidth = 5;
+//Global Variables 
+let y; //Storing the ground position it will start from
+let start, original_start; // movment through Perlin Noise when generating terrain
+let rectWidth = 5; // Width Of each terrain rectangle
 
-let panX = 0;
+let panX = 0; // Both controls horizontal panning
 let panSpeed = -1;
 
-let highestY; let highestX;
-let total_Height;
+let highestY; let highestX; // Storing the tallest/highest terrain peak
+
+let total_Height; //Variables that are being used to calculate the average terrain height
 let countHeight;
 let average;
 
 
-let tallestPeak;
+let tallestPeak; // Storing the Y position of the tallest peak found
 
 
 //-------------------------------------//
@@ -27,13 +28,15 @@ let tallestPeak;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  y = windowHeight;
-  original_start = random(100);
+  y = windowHeight; // Sets the terrain base to the bottom of the screen
+  original_start = random(100); // Random starting value
 
 }
 
 function backgroundColor() {
-   let topColor = color(22, 4, 244);
+  // This function allows to make a colorful gradient background
+
+  let topColor = color(22, 4, 244);
   let bottomColor = color(25, 200, 205);
 
   // Looping through every y-pixel
@@ -47,44 +50,57 @@ function backgroundColor() {
 }
 
 function generateTerrain() {
-  start = original_start;
-  tallestPeak = height;
+  // This function allows to generate a terrain with
+  // the use of Perlin Noise
+  // This function allows an addition feature to find
+  // average height of the terrain using a bright red line
 
-  let total_Height = 0;
+  start = original_start; // Resets the noise starting point each frame
+  tallestPeak = height; // Restes the tallest peak value
+
+  let total_Height = 0; // Resetting the calculations for average height
   let countHeight = 0;
 
   for (let x = 0; x < width; x += rectWidth) {
     let rectHeight = map(noise(start), 0, 1, 0, height * 0.95);
 
+    // Terrain Appearances
     fill(0,0, 120);
     stroke(0,50,250);
     strokeWeight(0.5);
+
+    // The Drawing of the Terrain Rectangle
     rect(x, y, rectWidth, -rectHeight);
+    
+    //Moving to the next noise value
     start += 0.005;
 
-    let tallestY = height - rectHeight;
+    let tallestY = height - rectHeight; // Calculation of the top of the terrain column
+    //Checking if the current column is the tallest peak so far 
     if (tallestY < tallestPeak) {
       tallestPeak = tallestY;
       highestX = x + rectWidth / 4;
       highestY = tallestY;
     }
+    // Adding height to total for average calculation
     total_Height += rectHeight;
     countHeight++;
       
   }
-
+  // Calculates Average Terrain Height
   average = total_Height / countHeight;
-  fill("red");
+  
+  fill("red"); // Draws thge line where the Average Height is
   rect(0, height - average, width, 3);
 
-  drawFlag(highestX, highestY);
+  drawFlag(highestX, highestY); // Draws the flag on the tallest peak
 }
 
 function keyPressed() {
-  if (keyCode === 37) {
+  if (keyCode === 37) { /// <- Compression of the graph
     rectWidth += 1;
   }
-  else if (keyCode === 39) {
+  else if (keyCode === 39) { /// <- Stretching of the graph
     rectWidth -= 1;
     if (rectWidth < 1) {
       rectWidth = 1;
@@ -93,21 +109,29 @@ function keyPressed() {
 }
 
 function drawFlag(x, y) {
+  // This function allows to draw a flag on top
+  // Of the highest peak in the terrain
+
+  // This creates the pole of the flag
   stroke(0,255,255);
   strokeWeight(6);
   line(x, y, x, y - 40);
 
+  // This creates the Canton and Fly End of the flag
   fill(0,250,255);
   strokeWeight(1)
   triangle(x - 1, y - 40, x - 1, y - 22, x + 30, y - 30);
 }
 
-function draw() {
-  backgroundColor();
+//------------------------------------------------------------//
+//-----------------------MAIN PROGRAM-------------------------//
+//------------------------------------------------------------//
 
-  original_start += 0.01;
-  panX += panSpeed;
-  generateTerrain();
+function draw() {
+  backgroundColor(); // --- Draws the gradient background
+  original_start += 0.01; // --- Move noise position to create scrolling terrain
+  panX += panSpeed; // --- Updates the horizontal panning of the canvas
+  generateTerrain(); // Generates terrain every frame
   
 
 }
