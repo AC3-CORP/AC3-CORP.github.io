09d13ccec2f50519eaf17ceaaf96c68730da45dd
AC3-CORP <ayeman2908@gmail.com>
2026-03-16 11:28:46 -0600

    Demo Mar 16/26

---
+++ 12 Demo Wihout Mr. Scott/sketch.js
@@ -0,0 +1,24 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+
+}
+
+function sing(){
+  console.log(" la la la la");
+  if (callback instanceof function){
+    callback();
+  }
+}
+
+function meow(){
+
+}
