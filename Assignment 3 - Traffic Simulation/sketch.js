0381054c6486b8317b34850147e74252e1ac4441
AC3-CORP <ayeman2908@gmail.com>
2026-04-02 11:35:55 -0600

    Fixed my Vehicle class methods, took out the AI dummy code which was being used to contrast with my wrong coding.
    I am almost done my comments on the coding, will be done by tonight

--- Assignment 3 - Traffic Simulation/sketch.js
+++ Assignment 3 - Traffic Simulation/sketch.js
@@ -4,24 +4,25 @@
 
 
 // Global Variables 
-let eastBound = [];
-let westBound = [];
-let light;
+let eastBound = []; // creates an array for the traffic going eastbound
+let westBound = []; // creates an array for the traffic going westbound
+let light; // creates a variable for the traffic light
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  light = new TrafficLight();
-  for (let i = 0; i < 20; i++) {
-    eastBound.push(new Vehicle(
+  light = new TrafficLight(); // sets up the new object in the variable
+
+  for (let i = 0; i < 20; i++) { //creates the cars for the traffic going eastbound
+    eastBound.push(new Vehicle( // a for loop to keep the flows cars unlimited
       random(width),
       random(height / 3 + 750, height / 3 + 250),
       1
     ));
   }
-  for (let i = 0; i < 20; i++) {
-    westBound.push(new Vehicle(
+  for (let i = 0; i < 20; i++) { //creates the cars for the traffic going westbound
+    westBound.push(new Vehicle( // a for loop to keep the flows cars unlimited
       random(width),
       random(height / 3 - 225, height / 3 + 188),
       0
@@ -30,16 +31,16 @@ function setup() {
 }
 
 function drawRoad() {
-  fill(0,10);
+  fill(0, 10);
   stroke("cyan ");
-  rect(0, 175, 1900, height-200);
+  rect(0, 175, 1900, height - 200);
 
   for (let x = 0; x < width; x += 45) {
     if (light.isRed) {
       stroke("red");
       strokeWeight(10);
       line(x, height / 2, x + 21, height / 2);
-    }else{
+    } else {
       stroke("cyan");
       strokeWeight(10);
       line(x, height / 2, x + 21, height / 2);
@@ -89,8 +90,9 @@ class Vehicle {
     this.direction = direction;
 
     this.type = int(random(2));
-    this.color = color(random(150,255),  15, random(200,255));
-    this.xSpeed = (direction === 1) ? random(2,5) : random(-2, -5);
+    this.color = color(random(150, 255), 15, random(200, 255));
+    this.xSpeed = (direction === 1);
+    this.random = int(random(101));
   }
 
   display() {
@@ -104,8 +106,15 @@ class Vehicle {
     rectMode(CENTER);
     noStroke();
 
-    let w = (this.type === 0) ? 50 : 75;
-    let h = (this.type === 0) ? 25 : 30;
+    let w, h;
+    if (this.type === 0) {
+      w = 50;
+      h = 25;
+    }
+    else{
+      w = 75;
+      h = 30;
+    }
 
     fill(this.color);
     rect(0, 0, w, h, 6);
@@ -127,99 +136,71 @@ class Vehicle {
 
   move() {
     if (light.isRed) return;
+    this.x += this.xSpeed;
 
-  let group = (this.direction === 1) ? eastBound : westBound;
-  let myLength = (this.type === 0) ? 50 : 75;
-  
-  let minGap = 30;
-  let closestDist = Infinity;
-  for (let other of group) {
-    if (other !== this) {
+    if (this.x > width) this.x = 0;
+    if (this.x < 0) this.x = width;
+  }
 
-      let otherLength = (other.type === 0) ? 50 : 75;
-
-      if (this.direction === 1 && other.x > this.x && abs(other.y - this.y) < 30) {
-        let d = other.x - this.x - (myLength/2 + otherLength/2);
-        if (d < closestDist) closestDist = d;
-      }
-
-      if (this.direction === 0 && other.x < this.x && abs(other.y - this.y) < 30) {
-        let d = this.x - other.x - (myLength/2 + otherLength/2);
-        if (d < closestDist) closestDist = d;
-      }
+  speedUp() {
+    if (this.direction === 1 && this.xSpeed < 7) {
+      this.xSpeed += 1;
+    }
+    if (this.direction === 0 && this.xSpeed > -7) {
+      this.xSpeed -= 1;
     }
   }
 
-  if (closestDist < minGap) {
-    this.speedDown();
-  } else {
-    this.speedUp();
+  speedDown() {
+    if (this.direction === 1 && this.xSpeed > 3) {
+      this.xSpeed -= 1;
+    }
+    if (this.direction === 0 && this.xSpeed < -3) {
+      this.xSpeed += 1;
+    }
   }
 
-  this.x += this.xSpeed/2;
-
-  if (this.x > width) this.x = 0;
-  if (this.x < 0) this.x = width;
-}
-
-speedUp() {
-  if (this.direction === 1 && this.xSpeed < 15) {
-    this.xSpeed += 1;
+  changeColor() {
+    this.color = color(random(15, 255), random(115), random(200, 255));
   }
-  if (this.direction === 0 && this.xSpeed > -15) {
-    this.xSpeed -= 1;
-  }
-}
 
-speedDown() {
-  if (this.direction === 1 && this.xSpeed > 0) {
-    this.xSpeed -= 0.5;
+  action() {
+    this.move();
+    this.random = int(random(101));
+    if (this.random === 1) this.speedUp();
+    if (this.random === 2) this.speedDown();
+    if (this.random === 3) this.changeColor();
+    this.display();
   }
-  if (this.direction === 0 && this.xSpeed < 0) {
-    this.xSpeed += 0.5;
-  }
-}
-
-changeColor() {
-  this.color = color(random(0,150), random(0,155), random(200,255));
-}
-
-action() {
-  this.move();
-  if (random(100) < 1) this.speedUp();
-  if (random(100) < 1) this.speedDown();
-  if (random(100) < 1) this.changeColor();
-  this.display();
-}
 }
 
 
 function mousePressed() {
   if (keyIsDown(SHIFT)) {
     westBound.push(new Vehicle(mouseX, random(height / 3 - 200, height / 3 + 200), 0));
-  } else{
-    eastBound.push(new Vehicle(mouseX, random(height / 3 + 750, height / 3 + 250),1));
+  } else {
+    eastBound.push(new Vehicle(mouseX, random(height / 3 + 750, height / 3 + 250), 1));
   }
 }
 
 function keyPressed() {
-  if (key === ' ') {
+  if (keyCode === 32 ) {
     light.turnRed();
   }
 }
 
 function draw() {
-  background(0,70);
+  background(0, 70);
   drawRoad();
 
   light.update();
   light.display();
 
-  for (let v of eastBound) {
-    v.action();
+  for (let eB of eastBound) {
+    eB.action();
   }
 
-  for (let v of westBound) {
-    v.action();
+  for (let wB of westBound) {
+    wB.action();
   }
 }
