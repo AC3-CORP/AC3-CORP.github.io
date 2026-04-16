8e51c39843685ba473c41d0aa1d036e2a7811d52
AC3-CORP <ayeman2908@gmail.com>
2026-04-16 12:18:49 -0600

    Updated demo to the assignment AND added the  CHEAT function

--- 18 2D Array Basics/sketch.js
+++ 18 2D Array Basics/sketch.js
@@ -1,4 +1,4 @@
-// 18 2D Arrays Basics
+// 18 2D Arrays Basics/ Puzzle Game Assignment
 // Ayeman Islam
 // April 15/26
 //
@@ -14,11 +14,11 @@ let grid = [
 ];
 let rows = grid.length;
 let cols = grid[0].length;
-let tileSize = 60;
-
+let tileSize = 150;
+let win = false;
 
 function setup() {
-  createCanvas(cols*tileSize, rows*tileSize);
+  createCanvas(cols * tileSize, rows * tileSize);
 }
 
 function draw() {
@@ -26,40 +26,68 @@ function draw() {
   renderGrid();
   textSize(30);
   fill("red");
-  text(getCurrentX() + ", " + getCurrentY(), mouseX, mouseY);
+  //text(getCurrentX() + ", " + getCurrentY(), mouseX, mouseY);
 }
 
 
-function mousePressed(){
-  flip(getCurrentX(), getCurrentY());
+function mousePressed() {
+  // only do a flip if mouse is on the canvas
+  if (mouseX < width && mouseY < height) {
+
+    let x = getCurrentX(); let y = getCurrentY();
+
+
+    if (keyIsDown(SHIFT)) { // CHEATER CHEATER CODE
+      flip(x, y);           // Helps to cheat through the puzzle
+    }
+    else {
+      // ALWAYS:
+      flip(x, y);
+
+      // Flip the cardinal (NSEW) neighbours
+      if (x - 1 >= 0) flip(x - 1, y); // WEST
+      if (x + 1 <= rows) flip(x + 1, y); // EAST
+      if (y - 1 >= 0) flip(x, y - 1); // NORTH
+      if (y + 1 <= cols) flip(x, y + 1); // SOUTH
+    }
+  }
 }
 
-function flip(x, y){
-  if(grid[y][x] === 0) grid[y][x] = 255;
+
+function flip(x, y) {
+  if (grid[y][x] === 0) grid[y][x] = 255;
   else grid[y][x] = 0;
 }
 
-function getCurrentX(){
+function getCurrentX() {
   // determine the current col pos of mouse
-  let constrainedX = constrain(mouseX, 0, width-1);
+  let constrainedX = constrain(mouseX, 0, width - 1);
   return floor(constrainedX / tileSize);
 }
 
-function getCurrentY(){
+function getCurrentY() {
   // determine the current row pos of mouse
-  let constrainedY = constrain(mouseY, 0, height-1);
+  let constrainedY = constrain(mouseY, 0, height - 1);
   return floor(constrainedY / tileSize);
 }
 
-function renderGrid(){
+function renderGrid() {
   // interpret the data stored in 2D array(grid) and
   // draw a matrix of squares to reflect it
 
-  for( let y = 0; y < rows; y++){ // y: 0 1 2 3 4 
-    for( let x = 0; x < cols; x++){ // x: 0 1 2 3 4 5
+  for (let y = 0; y < rows; y++) { // y: 0 1 2 3 4 
+    for (let x = 0; x < cols; x++) { // x: 0 1 2 3 4 5
       let fillColor = grid[y][x];
       fill(fillColor);
-      rect(x*tileSize, y*tileSize, tileSize);
+      rect(x * tileSize, y * tileSize, tileSize);
+    }
+  }
+
+  for (let y = 0; y < rows; y++) { // y: 0 1 2 3 4 
+    for (let x = 0; x < cols; x++) { // x: 0 1 2 3 4 5
+      if (grid[y][x] === 0 || grid[y][x] === 255){
+        win = true;
+      };
     }
   }
 }
